f71fe4d768b06f386f33ca20d44e5c641086fe57
Abdelrahman El-Adawy <adawy@oroboro.studio>
2020-05-07 00:25:48 +0200

    refactor server.js routes to be more readable and intuitive

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-
 const express = require('express');
 const app = express();
 const bcrypt = require('bcryptjs');
@@ -33,45 +32,52 @@ app.use(passport.session())
 app.use(methodOverride('_method'))
 
 
-app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index.ejs', { name: req.user.name });
-});
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login.ejs');
-});
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}));
-
-app.get('/register', checkNotAuthenticated, (req, res) => {
-  res.render('register.ejs');
-});
-
-//
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
-    });
-    res.redirect('/login');
-  } catch {
-    res.redirect('/register');
-  }
-  console.log(users);
-});
-
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
+app.route('/')
+  .get(checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name });
 })
+
+app.route('/login')
+
+  .get(checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs');
+  })
+
+  .post(checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
+
+
+app.route('/register')
+
+  .get(checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs');
+  })
+
+  .post(checkNotAuthenticated, async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      res.redirect('/login');
+    } catch {
+      res.redirect('/register');
+    }
+    console.log(users);
+  })
+
+app.route('/logout')
+
+  .delete((req, res) => {
+    req.logOut()
+    res.redirect('/login')
+  })
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
